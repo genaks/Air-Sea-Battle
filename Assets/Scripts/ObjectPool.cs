54a136bb9b2ec67e01ed67b25b447a9cb7eb5e87
Akshay Jain <a.jain@arts.ac.uk>
2021-07-12 15:17:34 +0100

    object pool set up

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    private List<GameObject> pooledObjects;
+    public GameObject objectToPool;
+    public int numberOfObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        GameObject newObject;
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            newObject = Instantiate(objectToPool);
+            newObject.transform.parent = transform;
+            newObject.SetActive(false);
+            pooledObjects.Add(newObject);
+        }
+    }
+
+    public GameObject GetObject()
+    {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        return null;
+    }
+
+    public void DestroyAllObjects()
+    {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            pooledObjects[i].SetActive(false);
+        }
+    }
+}
