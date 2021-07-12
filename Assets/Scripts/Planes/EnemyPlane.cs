9bf659cdd515dd57973a229ad27b8d1fb87f614b
Akshay Jain <a.jain@arts.ac.uk>
2021-07-12 15:48:42 +0100

    Enemy plane behaviour

---
+++ Assets/Scripts/Planes/EnemyPlane.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlane : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 20;
+    public PlanesController controller;
+    private const string bulletTag = "Bullet";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate((transform.right * speed * Time.deltaTime));
+        Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        if (!screenRect.Contains(transform.position))
+        {
+            controller.DestroyPlane();
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == bulletTag)
+        {
+            controller.OnBulletHit();
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Planes/PlanesController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanesController : MonoBehaviour
+{
+    [SerializeField]
+    private int minimumNumberOfPlanes = 3;
+    [SerializeField]
+    private int maximumNumberOfPlanes = 5;
+    [SerializeField]
+    private ObjectPool planesPool;
+
+    private int numberOfActivePlanes = 0;
+
+    [SerializeField]
+    private int defaultSpawnYOffset = 200;
+    [SerializeField]
+    private AudioSource audioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public IEnumerator SpawnPlanes()
+    {
+        int numberOfPlanes = Random.Range(minimumNumberOfPlanes, maximumNumberOfPlanes + 1);
+        for (int i = 0; i < numberOfPlanes; i++)
+        {
+            yield return new WaitForSeconds(1.0f);
+            GameObject plane = planesPool.GetObject();
+            if (plane != null)
+            {
+                plane.GetComponent<EnemyPlane>().controller = this;
+                plane.transform.position = GetSpawnPositionFor(plane, i);
+                plane.transform.rotation = transform.rotation;
+                plane.SetActive(true);
+            }
+        }
+        numberOfActivePlanes = numberOfPlanes;
+    }
+
+    private Vector2 GetSpawnPositionFor(GameObject plane, int index)
+    {
+        RectTransform rect = (RectTransform)plane.transform;
+        float height = rect.rect.height;
+
+        Vector2 spawnPosition = new Vector2(0, Screen.height - defaultSpawnYOffset - index * height);
+        return spawnPosition;
+    }
+
+    public void OnBulletHit()
+    {
+        audioSource.Play();
+        DestroyPlane();
+    }
+
+    public void DestroyPlane()
+    {
+        numberOfActivePlanes--;
+        if (numberOfActivePlanes == 0)
+        {
+            StartCoroutine(SpawnPlanes());
+        }
+    }
+
+    public void DestroyAllPlanes()
+    {
+        planesPool.DestroyAllObjects();
+    }
+}
