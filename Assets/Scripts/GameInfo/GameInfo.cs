cc466ffdae1bd85074ac1f350742135c1329ae35
Akshay Jain <a.jain@arts.ac.uk>
2021-07-12 15:03:36 +0100

    Game Info HTTP Request

---
+++ Assets/Scripts/GameInfo/GameInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class GameInfo
+{
+    [SerializeField] private int time_limit;
+    public int timeLimit { get { return time_limit; } }
+
+    [SerializeField] private int default_high_score;
+    public int defaultHighScore { get { return default_high_score; } }
+
+    [SerializeField] private int points_per_plane;
+    public int pointsPerPlane { get { return points_per_plane; } }
+
+    public string id;
+}
---
+++ Assets/Scripts/GameInfo/GameInfoFetcher.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameInfoFetcher : MonoBehaviour
+{
+    private const string API_URL = "http://content.gamefuel.info/api/client_programming_test/air_battle_v1/content/config/config";
+
+    private bool logging = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetRequest(API_URL));
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError || webRequest.isHttpError)
+            {
+                //Do nothing. We can start the game with the default values.
+                if (logging)
+                {
+                    Debug.Log("Error - " + webRequest.error);
+                }
+            }
+            else
+            {
+                string json = webRequest.downloadHandler.text;
+                GameInfo info = JsonUtility.FromJson<GameInfo>(json);
+                Log(info);
+            }
+        }
+    }
+
+    private void Log(GameInfo info)
+    {
+        if (logging)
+        {
+            Debug.Log("time limit - " + info.timeLimit);
+            Debug.Log("points - " + info.pointsPerPlane);
+            Debug.Log("score - " + info.defaultHighScore);
+        }
+    }
+}
