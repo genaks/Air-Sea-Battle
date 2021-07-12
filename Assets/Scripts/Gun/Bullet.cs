03818ff6c8963f45dc9f939f0df44646c7bc08f5
Akshay Jain <a.jain@arts.ac.uk>
2021-07-12 15:29:54 +0100

    Gun behaviour Bullet behaviour

--- Assets/Scripts/Gun/Bullet.cs
+++ Assets/Scripts/Gun/Bullet.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public int fireAngle = 60;
+    [SerializeField]
+    private float speed = 20;
+    private const string planeTag = "Plane";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        float a = fireAngle * Mathf.Deg2Rad;
+        Vector3 direction = (transform.up * Mathf.Sin(a) + transform.right * Mathf.Cos(a)).normalized;
+
+        transform.Translate((direction * speed * Time.deltaTime));
+        Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        if (!screenRect.Contains(transform.position)) //check if a bullet is inside the screen rect
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == planeTag)
+        {
+            gameObject.SetActive(false); //disable if collided with plane
+        }
     }
 }
--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -1,18 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
+    [SerializeField]
+    private int rotation;
+    [SerializeField]
+    private int maximumBullets = 5;
+
+    [SerializeField]
+    private Sprite defaultStateSprite;
+    [SerializeField]
+    private Sprite upStateSprite;
+    [SerializeField]
+    private Sprite downStateSprite;
+
+    [SerializeField]
+    private KeyCode fireKey = KeyCode.Space;
+    [SerializeField]
+    private KeyCode upKey = KeyCode.UpArrow;
+    [SerializeField]
+    private KeyCode downKey = KeyCode.DownArrow;
+
+    private Image image;
+    [SerializeField]
+    private ObjectPool bullets;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector2 position = transform.position;
+        position.x = Screen.width / 4;
+        transform.position = position;
+        image = GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(fireKey))
+        {
+            GameObject bullet = bullets.GetObject();
+            if (bullet != null)
+            {
+                bullet.GetComponent<Bullet>().fireAngle = rotation;
+                bullet.transform.position = transform.position;
+                bullet.transform.rotation = transform.rotation;
+                bullet.SetActive(true);
+            }
+        }
+        if (Input.GetKey(downKey))
+        {
+            rotation = 30;
+            image.sprite = downStateSprite;
+        }
+        else if (Input.GetKey(upKey))
+        {
+            rotation = 90;
+            image.sprite = upStateSprite;
+        }
+        else
+        {
+            rotation = 60;
+            image.sprite = defaultStateSprite;
+        }
+    }
+
+    public void DestroyAllBullets()
+    {
+        bullets.DestroyAllObjects();
     }
 }
